72a419930d19508f69cf3510b169189466bf60f2
Filip Witkowski <45639326+fwitkowski17@users.noreply.github.com>
2024-03-22 13:55:52 +0100

    added IndexController to index.ts

--- api/lib/index.ts
+++ api/lib/index.ts
@@ -3,6 +3,7 @@ import IndexController from './controllers/index.controller';
 import DataController from './controllers/data.controller';
 
 const app: App = new App([
+    new IndexController(),
     new DataController()
 ]);
 
